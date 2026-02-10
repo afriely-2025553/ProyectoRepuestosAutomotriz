8abce83f48a775d8e833ad6cbd707255f2a81e55
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-10 10:04:51 -0600

    feat: se guardaron cambios en el ProveedoresController

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/ProveedoresController.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/ProveedoresController.java
@@ -1,4 +1,11 @@
 package com.AlexFriely.RepuestosAutomoticez.Controllers;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Proveedores")
 public class ProveedoresController {
+
 }
