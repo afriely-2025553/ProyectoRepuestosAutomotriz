cdce2d2da86d6ead966635e80a357ddc167923ca
afriely-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-11 11:16:18 -0600

    feat: se agrego el createRepuesto

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/RepuestosController.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/RepuestosController.java
@@ -1,4 +1,35 @@
 package com.AlexFriely.RepuestosAutomoticez.Controllers;
 
+
+import com.AlexFriely.RepuestosAutomoticez.Entity.Repuestos;
+import com.AlexFriely.RepuestosAutomoticez.Service.RepuestosService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api//repuestos")
 public class RepuestosController {
+    private final RepuestosService repuestosService;
+
+
+    public RepuestosController(RepuestosService repuestosService) {
+        this.repuestosService = repuestosService;
+    }
+    @GetMapping
+    public ResponseEntity<Object> createProveedor(@Valid @RequestBody Repuestos repuestos){
+        try {
+            Repuestos createRepuestos = repuestosService.saveProveedor(repuestos);
+            return new ResponseEntity<>(createRepuestos, HttpStatus.CREATED);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    
 }
