f732a908609d5309a35a2df140391b56749e0e63
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-13 21:32:12 -0600

    feat: se agregaron las configuraciones a la interfce RepuestosRepository

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Repository/RepuestosRepository.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Repository/RepuestosRepository.java
@@ -1,4 +1,9 @@
 package com.AlexFriely.RepuestosAutomoticez.Repository;
 
-public interface RepuestosRepository {
+import com.AlexFriely.RepuestosAutomoticez.Entity.Repuestos;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RepuestosRepository extends JpaRepository<Repuestos, Integer> {
 }
