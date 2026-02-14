84cce0406fc6d2ff33b69ee387ff557ddaf61c05
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-13 21:26:36 -0600

    feat: se agregaron cambios en la clase RepuestosController

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/RepuestosController.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Controllers/RepuestosController.java
@@ -6,15 +6,12 @@ import com.AlexFriely.RepuestosAutomoticez.Service.RepuestosService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api//repuestos")
+@RequestMapping("/api/repuestos")
 public class RepuestosController {
     private final RepuestosService repuestosService;
 
@@ -22,12 +19,49 @@ public class RepuestosController {
     public RepuestosController(RepuestosService repuestosService) {
         this.repuestosService = repuestosService;
     }
+
     @GetMapping
+    public List<Repuestos> getAllProveedores(){
+        return repuestosService.getAllRespuestos();
+    }
+
+
+    @PostMapping
     public ResponseEntity<Object> createProveedor(@Valid @RequestBody Repuestos repuestos){
         try {
             Repuestos createRepuestos = repuestosService.saveProveedor(repuestos);
             return new ResponseEntity<>(createRepuestos, HttpStatus.CREATED);
         }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateRepuestos(@PathVariable Integer id,@Valid @RequestBody Repuestos repuestos){
+        try {
+            Repuestos updateRepuestos = repuestosService.updateRepuestos(id, repuestos);
+            return new ResponseEntity<>(updateRepuestos, HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getRepuestosById(@PathVariable Integer id, Repuestos repuestos) {
+        try{
+            Repuestos searchRepuestos = repuestosService.getRepuestosById(id);
+            return new ResponseEntity<>(searchRepuestos,HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteRepuestos(@PathVariable Integer id){
+        try {
+            repuestosService.deleteRepuestos(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
