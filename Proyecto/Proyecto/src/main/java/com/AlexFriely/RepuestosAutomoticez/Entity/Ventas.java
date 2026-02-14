8877ab3e8ba57512f1af3a69d6ab72bb24b8b112
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-14 02:09:06 -0600

    feat: se agregaron cambios a Ventas

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Entity/Ventas.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Entity/Ventas.java
@@ -1,4 +1,81 @@
 package com.AlexFriely.RepuestosAutomoticez.Entity;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Ventas")
 public class Ventas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_venta")
+    private Integer id_venta;
+
+    @Column(name = "fecha_venta")
+    private Date fecha_venta;
+
+    @Column(name = "cantidad")
+    private Integer cantidad;
+
+    @Column(name = "total")
+    private Double total;
+
+    @Column(name = "id_empleado")
+    private Integer id_empleado;
+
+    @Column(name = "id_repuesto")
+    private Integer id_repuesto;
+
+    //generar getters and setters
+
+
+    public Integer getId_venta() {
+        return id_venta;
+    }
+
+    public Date getFecha_venta() {
+        return fecha_venta;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public Integer getId_empleado() {
+        return id_empleado;
+    }
+
+    public Integer getId_repuesto() {
+        return id_repuesto;
+    }
+
+    public void setId_venta(Integer id_venta) {
+        this.id_venta = id_venta;
+    }
+
+    public void setFecha_venta(Date fecha_venta) {
+        this.fecha_venta = fecha_venta;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public void setId_empleado(Integer id_empleado) {
+        this.id_empleado = id_empleado;
+    }
+
+    public void setId_repuesto(Integer id_repuesto) {
+        this.id_repuesto = id_repuesto;
+    }
 }
