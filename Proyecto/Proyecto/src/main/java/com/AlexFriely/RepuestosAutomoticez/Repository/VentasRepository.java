c2fa6396b95b91940d9e6165ea9d6002f7ecfbe2
afrily-2025553 <afriely-2025553@kinal.edu.gt>
2026-02-14 02:09:59 -0600

    feat: se agrego la configuracion a la interface VentasRepository

--- Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Repository/VentasRepository.java
+++ Proyecto/Proyecto/src/main/java/com/AlexFriely/RepuestosAutomoticez/Repository/VentasRepository.java
@@ -1,4 +1,10 @@
 package com.AlexFriely.RepuestosAutomoticez.Repository;
 
-public interface VentasRepository {
+
+import com.AlexFriely.RepuestosAutomoticez.Entity.Ventas;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentasRepository  extends JpaRepository<Ventas, Integer> {
 }
